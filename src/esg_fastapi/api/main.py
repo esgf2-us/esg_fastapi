dfbf73507c8c355c3462a51903e20da9d29e4932
Zach Price <pricezt@ornl.gov>
2025-06-23 11:27:36 -0400

    Handle HTTP status errors returned from Globus Search

--- src/esg_fastapi/api/main.py
+++ src/esg_fastapi/api/main.py
@@ -1,10 +1,11 @@
 """This module defines the main FastAPI application factory."""
 
 from fastapi import FastAPI
+from httpx import HTTPStatusError
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from starlette.routing import Mount
 
-from esg_fastapi.api.versions.v1.globus import FastAPIWithSearchClient, handle_upstream_connection_error, token_renewal_watchdog
+from esg_fastapi.api.versions.v1.globus import FastAPIWithSearchClient, handle_upstream_http_status_error, handle_upstream_timeout, token_renewal_watchdog
 from esg_fastapi.observability.main import observe
 
 
@@ -31,6 +32,8 @@ def app_factory() -> FastAPIWithSearchClient:
             api.router.include_router(route.app.router)
             api.router.tags.extend(route.app.router.tags)
 
-    api.add_exception_handler(TimeoutError, handle_upstream_connection_error)
+    api.add_exception_handler(TimeoutError, handle_upstream_timeout)
+    api.add_exception_handler(HTTPStatusError, handle_upstream_http_status_error)
+
     observe(api)
     return api
